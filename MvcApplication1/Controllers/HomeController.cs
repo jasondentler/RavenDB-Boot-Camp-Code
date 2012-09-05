2b1403e8eb3350c2a4a49bb0d8fc4d9865bd4a38
Patrick Lioi <patrick@headspring.com>
2012-09-05 10:36:07 -0500

    step 1 working

---
+++ MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult CreateStudent()
+        {
+            using (var documentSession = MvcApplication.DocumentStore.OpenSession())
+            {
+                documentSession.Store(new Student { Name = "Patrick", Course = "RavenDB Boot Camp", Date = DateTime.UtcNow });
+
+
+                documentSession.SaveChanges();
+            }
+
+            return RedirectToAction("ListStudents");
+        }
+
+        public ActionResult ListStudents()
+        {
+            using (var documentSession = MvcApplication.DocumentStore.OpenSession())
+            {
+                var results = documentSession.Query<Student>().ToList();
+                return Json(results, JsonRequestBehavior.AllowGet);
+            }            
+        }
+    }
+
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Course { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ MvcApplication1/Global.asax.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Raven.Client;
+using Raven.Client.Document;
+
+namespace MvcApplication1
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        //One DocumentStore per app!
+        public static IDocumentStore DocumentStore { get; private set; } 
+
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Default", // Route name
+                "{controller}/{action}/{id}", // URL with parameters
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+            );
+
+        }
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterRoutes(RouteTable.Routes);
+
+            InitializeDocumentStore();
+        }
+
+        private void InitializeDocumentStore()
+        {
+            DocumentStore = new DocumentStore {Url = "http://localhost:8080", DefaultDatabase = "BasicOps"};
+            DocumentStore.Initialize();
+        }
+    }
+}
